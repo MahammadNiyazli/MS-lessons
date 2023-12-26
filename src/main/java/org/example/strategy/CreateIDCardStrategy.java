faaa3a5b50fb27a11e42a82b020c867e7a779ab5
Mahammad Niyazli <mahammad.niyazli@gmail.com>
2023-12-26 19:24:53 +0400

    added new println data

--- src/main/java/org/example/strategy/CreateIDCardStrategy.java
+++ src/main/java/org/example/strategy/CreateIDCardStrategy.java
@@ -31,5 +31,6 @@ public class CreateIDCardStrategy implements AbstractCardStrategy{
 
         DB.addIdCard(idCard);
         System.err.println("Yeni sexsiyyet vesiqesi yaradildi");
+        System.err.println("Just change");
     }
 }
